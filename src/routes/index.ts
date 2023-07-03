dbda2938e5c7d2c2f65b9a63c59f944453252d79
cesarhenrq <cesaarhvb@gmail.com>
2023-07-02 22:04:18 -0300

    feat: add req info middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import reqInfoMiddleware from "../common/middlewares/req-info.middleware";
+
 const routes = Router();
+
+routes.use(reqInfoMiddleware);
 
 export default routes;
