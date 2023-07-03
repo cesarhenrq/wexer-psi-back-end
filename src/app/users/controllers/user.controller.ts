8d201b28a8f9f084aca935b64295291062d65f16
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 20:31:57 -0300

    feat: add update

--- src/app/users/controllers/user.controller.ts
+++ src/app/users/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import UserService from "../services/user.service";
 
 import userSchema from "../schemas/user.schema";
+import updateUserSchema from "../schemas/update-user.schema";
 
 export default class UserController {
   constructor(private service: UserService) {}
@@ -31,6 +32,35 @@ export default class UserController {
     const payload = { ...body, image: { filename, mimetype } };
 
     const { status, message, data } = await this.service.create(payload);
+
+    return res.status(status).json({
+      message,
+      data,
+    });
+  }
+
+  async update(req: Request, res: Response) {
+    const { body, file } = req;
+
+    const { id } = req.params;
+
+    try {
+      await updateUserSchema.validate(body);
+    } catch (err: any) {
+      return res.status(400).json({
+        message: err.errors,
+        data: null,
+      });
+    }
+
+    let payload = { ...body };
+
+    if (file) {
+      const { filename, mimetype } = file;
+      payload = { ...payload, image: { filename, mimetype } };
+    }
+
+    const { status, message, data } = await this.service.update(id, payload);
 
     return res.status(status).json({
       message,
