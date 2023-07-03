a45e1da0ad346f025ca2ec46c73bc88ebbbc9794
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 00:58:53 -0300

    feat: implement user module

---
+++ src/app/users/user.module.ts
@@ -0,0 +1,19 @@
+import User from "./entities/user.entity";
+
+import UserRepository from "./repositories/user.repository";
+
+import UserService from "./services/user.service";
+
+import UserController from "./controllers/user.controller";
+
+import FileModule from "../files/file.module";
+
+export default class UserModule {
+  static build() {
+    const repository = new UserRepository(User);
+    const service = new UserService(repository, FileModule.build().repository);
+    const controller = new UserController(service);
+
+    return { controller };
+  }
+}
