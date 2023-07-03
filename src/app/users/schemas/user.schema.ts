17408688deb3cf5ed4363179399c7e415fcd42ad
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 00:59:48 -0300

    refactor: delete image validation

--- src/app/users/schemas/user.schema.ts
+++ src/app/users/schemas/user.schema.ts
@@ -4,7 +4,6 @@ const userSchema = object().shape({
   name: string().required(),
   email: string().email().required(),
   password: string().min(6).required(),
-  image: string().required(),
 });
 
 export default userSchema;
