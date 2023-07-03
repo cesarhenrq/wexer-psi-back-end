7e658ac1c2d27e32c62ff845087eb6e128aa32a5
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 17:37:27 -0300

    feat: add post method

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const authRouter = Router();
 
+import AuthModule from "../app/auth/auth.module";
+
+const authController = AuthModule.build().controller;
+
+authRouter.post("/", authController.login.bind(authController));
+
 export default authRouter;
