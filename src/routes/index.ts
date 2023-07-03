214ff116a6dcdfc631a6663540414aac43bc33e0
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 01:01:23 -0300

    feat: add user router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 import reqInfoMiddleware from "../common/middlewares/req-info.middleware";
 
+import userRouter from "./user.routes";
+
 const routes = Router();
 
 routes.use(reqInfoMiddleware);
+
+routes.use("/users", userRouter);
 
 export default routes;
