d210a43fa9c6191fde2d67093bd84ed91cbed2c6
cesarhenrq <cesaarhvb@gmail.com>
2023-07-02 22:58:50 -0300

    feat: add find by email

--- src/app/users/repositories/user.repository.ts
+++ src/app/users/repositories/user.repository.ts
@@ -2,4 +2,8 @@ import User from "../entities/user.entity";
 
 export default class UserRepository {
   constructor(private model: typeof User) {}
+
+  async findByEmail(email: string) {
+    return this.model.findOne({ email }).populate("image");
+  }
 }
