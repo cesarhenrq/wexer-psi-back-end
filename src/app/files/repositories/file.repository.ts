2dc64492cf383037db88209bb5b8215135e6cdbf
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 20:31:25 -0300

    feat: add update

--- src/app/files/repositories/file.repository.ts
+++ src/app/files/repositories/file.repository.ts
@@ -8,4 +8,8 @@ export default class FileRepository {
   async create(file: CreateFileDto) {
     return await this.model.create(file);
   }
+
+  async update(id: string, file: CreateFileDto) {
+    return await this.model.findByIdAndUpdate(id, file, { new: true });
+  }
 }
