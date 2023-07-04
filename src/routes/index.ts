63b38af033222badd8a1dd92ea2eac4f4e1b01a1
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 23:51:42 -0300

    feat: add patient router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import reqInfoMiddleware from "../common/middlewares/req-info.middleware";
 
 import userRouter from "./user.routes";
 import authRouter from "./auth.routes";
+import patientRouter from "./patient.routes";
 
 const routes = Router();
 
@@ -12,5 +13,7 @@ routes.use(reqInfoMiddleware);
 routes.use("/users", userRouter);
 
 routes.use("/auth", authRouter);
+
+routes.use("/patients", patientRouter);
 
 export default routes;
