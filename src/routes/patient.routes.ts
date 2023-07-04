830b8d5f01e41f01bd82d49c9ca615419b002ccb
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 23:51:17 -0300

    feat: add create patient route

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -2,4 +2,14 @@ import { Router } from "express";
 
 const patientRouter = Router();
 
+import AuthMiddleware from "../common/middlewares/auth.middleware";
+
+import PatientModule from "../app/patients/patient.module";
+
+const patientController = PatientModule.build().controller;
+
+patientRouter.use(AuthMiddleware.execute.bind(AuthMiddleware));
+
+patientRouter.post("/", patientController.create.bind(patientController));
+
 export default patientRouter;
