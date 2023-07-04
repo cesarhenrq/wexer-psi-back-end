08f637874d36ab4c7a1eeb777de9e5ccf9452b84
cesarhenrq <cesaarhvb@gmail.com>
2023-07-03 23:41:10 -0300

    feat: add create

--- src/app/patients/controllers/patient.controller.ts
+++ src/app/patients/controllers/patient.controller.ts
@@ -1,5 +1,26 @@
+import { Request, Response } from "express";
+
 import PatientService from "../services/patient.service";
+
+import createPatientSchema from "../schemas/create-patient.schema";
 
 export default class PatientController {
   constructor(private service: PatientService) {}
+
+  async create(req: Request, res: Response) {
+    const { body } = req;
+
+    try {
+      await createPatientSchema.validate(body);
+    } catch (err: any) {
+      return res.status(400).json({ message: err.message, data: null });
+    }
+
+    const { status, message, data } = await this.service.create(body);
+
+    return res.status(status).json({
+      message,
+      data,
+    });
+  }
 }
