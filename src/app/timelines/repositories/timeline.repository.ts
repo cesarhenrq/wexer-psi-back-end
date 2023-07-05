07e92af310782f777c03af2c7b03389274f527dd
cesarhenrq <cesaarhvb@gmail.com>
2023-07-05 20:07:36 -0300

    feat: add create

--- src/app/timelines/repositories/timeline.repository.ts
+++ src/app/timelines/repositories/timeline.repository.ts
@@ -1,5 +1,11 @@
 import Timeline from "../entities/timeline.entity";
 
+import { CreateTimelineDto } from "../dtos/create-timeline.dto";
+
 export default class TimelineRepository {
   constructor(private model: typeof Timeline) {}
+
+  async create(timeline: CreateTimelineDto) {
+    return await this.model.create(timeline);
+  }
 }
