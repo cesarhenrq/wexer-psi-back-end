cd2e6b8d98c4c45f9ad1d0e08818039e76027aa6
cesarhenrq <cesaarhvb@gmail.com>
2023-07-05 20:09:16 -0300

    feat: add create endpoint

--- src/routes/timeline.route.ts
+++ src/routes/timeline.route.ts
@@ -2,4 +2,14 @@ import { Router } from "express";
 
 const timelineRouter = Router();
 
+import AuthMiddleware from "../common/middlewares/auth.middleware";
+
+import TimelineModule from "../app/timelines/timeline.module";
+
+const timelineController = TimelineModule.build().controller;
+
+timelineRouter.use(AuthMiddleware.execute.bind(AuthMiddleware));
+
+timelineRouter.post("/:id", timelineController.create.bind(timelineController));
+
 export default timelineRouter;
