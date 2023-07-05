72bfe0d2f4aece6c8824f4c76c4a185092184803
cesarhenrq <cesaarhvb@gmail.com>
2023-07-05 20:08:01 -0300

    feat: add create

--- src/app/timelines/services/patient.service.ts
+++
@@ -1,9 +0,0 @@
-import TimelineRepository from "../repositories/timeline.repository";
-import PatientRepository from "../../patients/repositories/patient.repository";
-
-export default class TimelineService {
-  constructor(
-    private timelineRepository: TimelineRepository,
-    private patientRepository: PatientRepository
-  ) {}
-}
---
+++ src/app/timelines/services/timeline.service.ts
@@ -0,0 +1,44 @@
+import TimelineRepository from "../repositories/timeline.repository";
+import PatientRepository from "../../patients/repositories/patient.repository";
+
+import { CreateTimelineDto } from "../dtos/create-timeline.dto";
+
+export default class TimelineService {
+  constructor(
+    private timelineRepository: TimelineRepository,
+    private patientRepository: PatientRepository
+  ) {}
+
+  async create(patientId: string, payload: CreateTimelineDto) {
+    try {
+      const patient = await this.patientRepository.findById(patientId);
+
+      if (!patient) {
+        return {
+          status: 404,
+          message: "Patient not found",
+          data: null,
+        };
+      }
+
+      const timeline = await this.timelineRepository.create(payload);
+
+      await this.patientRepository.associateTimeline(
+        patientId,
+        timeline._id as unknown as string
+      );
+
+      return {
+        status: 201,
+        message: "Timeline created",
+        data: timeline,
+      };
+    } catch (err) {
+      return {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+    }
+  }
+}
