b0d664383814e211c62a3ad55ba5bcac7a190112
cesarhenrq <cesaarhvb@gmail.com>
2023-07-05 18:51:18 -0300

    feat: add update

--- src/app/patients/controllers/patient.controller.ts
+++ src/app/patients/controllers/patient.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import PatientService from "../services/patient.service";
 
 import createPatientSchema from "../schemas/create-patient.schema";
+import updatePatientSchema from "../schemas/update-patient.schema";
 
 export default class PatientController {
   constructor(private service: PatientService) {}
@@ -13,7 +14,7 @@ export default class PatientController {
     try {
       await createPatientSchema.validate(body);
     } catch (err: any) {
-      return res.status(400).json({ message: err.message, data: null });
+      return res.status(400).json({ message: err.errors, data: null });
     }
 
     const { status, message, data } = await this.service.create(body);
@@ -28,6 +29,24 @@ export default class PatientController {
     const { id } = req.params;
 
     const { status, message, data } = await this.service.findById(id);
+
+    return res.status(status).json({
+      message,
+      data,
+    });
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { body } = req;
+
+    try {
+      await updatePatientSchema.validate(body);
+    } catch (err: any) {
+      return res.status(400).json({ message: err.errors, data: null });
+    }
+
+    const { status, message, data } = await this.service.update(id, body);
 
     return res.status(status).json({
       message,
