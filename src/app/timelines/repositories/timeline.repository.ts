eed5b0bf3d2f9c25f62aea359e5c8b4d59708a4e
cesarhenrq <cesaarhvb@gmail.com>
2023-07-05 21:34:01 -0300

    feat: add update

--- src/app/timelines/repositories/timeline.repository.ts
+++ src/app/timelines/repositories/timeline.repository.ts
@@ -1,6 +1,7 @@
 import Timeline from "../entities/timeline.entity";
 
 import { CreateTimelineDto } from "../dtos/create-timeline.dto";
+import { UpdateTimelineDto } from "../dtos/update-timeline.dto";
 
 export default class TimelineRepository {
   constructor(private model: typeof Timeline) {}
@@ -11,5 +12,9 @@ export default class TimelineRepository {
 
   async findById(id: string) {
     return await this.model.findById(id);
+  }
+
+  async update(id: string, timeline: UpdateTimelineDto) {
+    return this.model.findByIdAndUpdate(id, timeline, { new: true });
   }
 }
