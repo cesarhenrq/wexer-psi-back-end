07ab66cd901df5f30a497377d19b35ba603353fc
cesarhenrq <cesaarhvb@gmail.com>
2023-07-07 20:24:27 -0300

    feat: add create occurrence end point

--- src/routes/occurrence.routes.ts
+++ src/routes/occurrence.routes.ts
@@ -2,4 +2,19 @@ import { Router } from "express";
 
 const occurrenceRouter = Router();
 
+import AuthMiddleware from "../common/middlewares/auth.middleware";
+import upload from "../configs/storage-config";
+
+import OccurrenceModule from "../app/occurrences/occurrence.module";
+
+const occurrenceController = OccurrenceModule.build().controller;
+
+occurrenceRouter.use(AuthMiddleware.execute.bind(AuthMiddleware));
+
+occurrenceRouter.post(
+  "/:id",
+  upload.array("files"),
+  occurrenceController.create.bind(occurrenceController)
+);
+
 export default occurrenceRouter;
