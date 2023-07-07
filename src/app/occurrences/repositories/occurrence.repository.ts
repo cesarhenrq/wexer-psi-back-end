0cc4a8a9e9ce8025eec0a63252f7b21688fa56dd
cesarhenrq <cesaarhvb@gmail.com>
2023-07-07 18:55:30 -0300

    feat: add create

--- src/app/occurrences/repositories/occurrence.repository.ts
+++ src/app/occurrences/repositories/occurrence.repository.ts
@@ -1,5 +1,11 @@
 import Occurrence from "../entities/occurrence.entity";
 
+import { CreateOccurrenceDto } from "../dtos/create-occurrence.dto";
+
 export default class OccurrenceRepository {
   constructor(private model: typeof Occurrence) {}
+
+  async create(occurrence: CreateOccurrenceDto) {
+    return this.model.create(occurrence);
+  }
 }
