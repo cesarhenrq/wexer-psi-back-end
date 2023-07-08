023e0819e8a1d27fc99e0d1759f75e7a5923e929
cesarhenrq <cesaarhvb@gmail.com>
2023-07-08 20:47:48 -0300

    feat: add update

--- src/app/occurrences/controllers/occurrence.controller.ts
+++ src/app/occurrences/controllers/occurrence.controller.ts
@@ -3,6 +3,7 @@ import OccurrenceService from "../services/occurrence.service";
 import { Request, Response } from "express";
 
 import createOccurrenceSchema from "../schemas/create-occurrence.schema";
+import updateOccurrenceSchema from "../schemas/update-occurrence.schema";
 
 export default class OccurrenceController {
   constructor(private service: OccurrenceService) {}
@@ -43,6 +44,38 @@ export default class OccurrenceController {
     const { id } = req.params;
 
     const { status, message, data } = await this.service.findById(id);
+
+    return res.status(status).json({
+      message,
+      data,
+    });
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    let { body, files } = req;
+
+    try {
+      await updateOccurrenceSchema.validate(body);
+    } catch (err: any) {
+      return res.status(400).json({
+        message: err.errors,
+        data: null,
+      });
+    }
+
+    let payload = { ...body };
+
+    if (files) {
+      files = (files as []).map((file: any) => ({
+        filename: file.filename,
+        mimetype: file.mimetype,
+      })) as [];
+
+      payload = { ...payload, files };
+    }
+
+    const { status, message, data } = await this.service.update(id, payload);
 
     return res.status(status).json({
       message,
