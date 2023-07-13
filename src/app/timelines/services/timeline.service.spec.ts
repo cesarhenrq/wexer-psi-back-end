eea965dc133db360ed2026310bae360227456715
cesarhenrq <cesaarhvb@gmail.com>
2023-07-13 20:52:23 -0300

    refactor: add more one describe to method

--- src/app/timelines/services/timeline.service.spec.ts
+++ src/app/timelines/services/timeline.service.spec.ts
@@ -31,7 +31,7 @@ describe("Timeline service", () => {
     mockedPatientRepository
   );
 
-  it("Should create a timeline", async () => {
+  describe("Create a timeline", () => {
     const patient = {
       _id: "patient-id",
       name: "Patient 1",
@@ -46,105 +46,167 @@ describe("Timeline service", () => {
       name: "Timeline 1",
     };
 
-    mockedPatientRepository.findById.mockResolvedValue(patient);
+    it("Should create a timeline", async () => {
+      mockedPatientRepository.findById.mockResolvedValue(patient);
 
-    mockedTimelineRepository.create.mockResolvedValue(timeline);
+      mockedTimelineRepository.create.mockResolvedValue(timeline);
 
-    const expected = {
-      status: 201,
-      message: "Timeline created",
-      data: timeline,
-    };
+      const expected = {
+        status: 201,
+        message: "Timeline created",
+        data: timeline,
+      };
 
-    const result = await sut.create("patient-id", payload);
+      const result = await sut.create("patient-id", payload);
 
-    expect(result).toEqual(expected);
-    expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
-    expect(mockedPatientRepository.findById).toBeCalledTimes(1);
-    expect(mockedTimelineRepository.create).toBeCalledWith(payload);
-    expect(mockedTimelineRepository.create).toBeCalledTimes(1);
-    expect(mockedPatientRepository.associateTimeline).toBeCalledWith(
-      "patient-id",
-      "timeline-id"
-    );
-    expect(mockedPatientRepository.associateTimeline).toBeCalledTimes(1);
+      expect(result).toEqual(expected);
+      expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
+      expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+      expect(mockedTimelineRepository.create).toBeCalledWith(payload);
+      expect(mockedTimelineRepository.create).toBeCalledTimes(1);
+      expect(mockedPatientRepository.associateTimeline).toBeCalledWith(
+        "patient-id",
+        "timeline-id"
+      );
+      expect(mockedPatientRepository.associateTimeline).toBeCalledTimes(1);
+    });
+
+    it("Should return 404 if patient is not found during", async () => {
+      mockedPatientRepository.findById.mockResolvedValue(null);
+
+      const expected = {
+        status: 404,
+        message: "Patient not found",
+        data: null,
+      };
+
+      const result = await sut.create("patient-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
+      expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+    });
+
+    it("Should return 500 if an error occurs in patientRepository.findById", async () => {
+      mockedPatientRepository.findById.mockRejectedValue(new Error());
+
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+
+      const result = await sut.create("patient-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
+      expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+    });
+
+    it("Should return 500 if an error occurs in timelineRepository.create", async () => {
+      mockedPatientRepository.findById.mockResolvedValue(patient);
+
+      mockedTimelineRepository.create.mockRejectedValue(new Error());
+
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+
+      const result = await sut.create("patient-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
+      expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+      expect(mockedTimelineRepository.create).toBeCalledWith(payload);
+      expect(mockedTimelineRepository.create).toBeCalledTimes(1);
+    });
+
+    it("Should return 500 if an error occurs in patientRepository.associateTimeline", async () => {
+      mockedPatientRepository.findById.mockResolvedValue(patient);
+
+      mockedTimelineRepository.create.mockResolvedValue(timeline);
+
+      mockedPatientRepository.associateTimeline.mockRejectedValue(new Error());
+
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+
+      const result = await sut.create("patient-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
+      expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+      expect(mockedTimelineRepository.create).toBeCalledWith(payload);
+      expect(mockedTimelineRepository.create).toBeCalledTimes(1);
+      expect(mockedPatientRepository.associateTimeline).toBeCalledWith(
+        "patient-id",
+        "timeline-id"
+      );
+      expect(mockedPatientRepository.associateTimeline).toBeCalledTimes(1);
+    });
   });
 
-  it("Should return 404 if patient is not found during create a timeline", async () => {
-    const payload = {
+  describe("Find a timeline", () => {
+    const timeline = {
+      _id: "timeline-id",
       name: "Timeline 1",
     };
 
-    mockedPatientRepository.findById.mockResolvedValue(null);
+    it("Should return a timeline", async () => {
+      mockedTimelineRepository.findById.mockResolvedValue(timeline);
 
-    const expected = {
-      status: 404,
-      message: "Patient not found",
-      data: null,
-    };
+      const expected = {
+        status: 200,
+        message: "Timeline found",
+        data: timeline,
+      };
 
-    const result = await sut.create("patient-id", payload);
+      const result = await sut.findById("timeline-id");
 
-    expect(result).toEqual(expected);
-    expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
-    expect(mockedPatientRepository.findById).toBeCalledTimes(1);
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+    });
+
+    it("Should return 404 if timeline is not found", async () => {
+      mockedTimelineRepository.findById.mockResolvedValue(null);
+
+      const expected = {
+        status: 404,
+        message: "Timeline not found",
+        data: null,
+      };
+
+      const result = await sut.findById("timeline-id");
+
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+    });
+
+    it("Should return 500 if an error occurs in timelineRepository.findById", async () => {
+      mockedTimelineRepository.findById.mockRejectedValue(new Error());
+
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+
+      const result = await sut.findById("timeline-id");
+
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+    });
   });
 
-  it("Should return 500 if an error occurs in patientRepository.findById during create a timeline", async () => {
-    const payload = {
-      name: "Timeline 1",
-    };
-
-    mockedPatientRepository.findById.mockRejectedValue(new Error());
-
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
-
-    const result = await sut.create("patient-id", payload);
-
-    expect(result).toEqual(expected);
-    expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
-    expect(mockedPatientRepository.findById).toBeCalledTimes(1);
-  });
-
-  it("Should return 500 if an error occurs in timelineRepository.create during create a timeline", async () => {
-    const patient = {
-      _id: "patient-id",
-      name: "Patient 1",
-    };
-
-    const payload = {
-      name: "Timeline 1",
-    };
-
-    mockedPatientRepository.findById.mockResolvedValue(patient);
-
-    mockedTimelineRepository.create.mockRejectedValue(new Error());
-
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
-
-    const result = await sut.create("patient-id", payload);
-
-    expect(result).toEqual(expected);
-    expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
-    expect(mockedPatientRepository.findById).toBeCalledTimes(1);
-    expect(mockedTimelineRepository.create).toBeCalledWith(payload);
-    expect(mockedTimelineRepository.create).toBeCalledTimes(1);
-  });
-
-  it("Should return 500 if an error occurs in patientRepository.associateTimeline during create a timeline", async () => {
-    const patient = {
-      _id: "patient-id",
-      name: "Patient 1",
-    };
-
+  describe("Update a timeline", () => {
     const payload = {
       name: "Timeline 1",
     };
@@ -154,160 +216,65 @@ describe("Timeline service", () => {
       name: "Timeline 1",
     };
 
-    mockedPatientRepository.findById.mockResolvedValue(patient);
+    it("Should update a timeline", async () => {
+      mockedTimelineRepository.update.mockResolvedValue(timeline);
 
-    mockedTimelineRepository.create.mockResolvedValue(timeline);
+      const expected = {
+        status: 200,
+        message: "Timeline updated",
+        data: timeline,
+      };
 
-    mockedPatientRepository.associateTimeline.mockRejectedValue(new Error());
+      const result = await sut.update("timeline-id", payload);
 
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.update).toBeCalledWith(
+        "timeline-id",
+        payload
+      );
+      expect(mockedTimelineRepository.update).toBeCalledTimes(1);
+    });
 
-    const result = await sut.create("patient-id", payload);
+    it("Should return 404 if timeline is not found", async () => {
+      mockedTimelineRepository.update.mockResolvedValue(null);
 
-    expect(result).toEqual(expected);
-    expect(mockedPatientRepository.findById).toBeCalledWith("patient-id");
-    expect(mockedPatientRepository.findById).toBeCalledTimes(1);
-    expect(mockedTimelineRepository.create).toBeCalledWith(payload);
-    expect(mockedTimelineRepository.create).toBeCalledTimes(1);
-    expect(mockedPatientRepository.associateTimeline).toBeCalledWith(
-      "patient-id",
-      "timeline-id"
-    );
-    expect(mockedPatientRepository.associateTimeline).toBeCalledTimes(1);
+      const expected = {
+        status: 404,
+        message: "Timeline not found",
+        data: null,
+      };
+
+      const result = await sut.update("timeline-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.update).toBeCalledWith(
+        "timeline-id",
+        payload
+      );
+      expect(mockedTimelineRepository.update).toBeCalledTimes(1);
+    });
+
+    it("Should return 500 if an error occurs in timelineRepository.update", async () => {
+      mockedTimelineRepository.update.mockRejectedValue(new Error());
+
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+
+      const result = await sut.update("timeline-id", payload);
+
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.update).toBeCalledWith(
+        "timeline-id",
+        payload
+      );
+      expect(mockedTimelineRepository.update).toBeCalledTimes(1);
+    });
   });
 
-  it("Should return a timeline", async () => {
-    const timeline = {
-      _id: "timeline-id",
-      name: "Timeline 1",
-    };
-
-    mockedTimelineRepository.findById.mockResolvedValue(timeline);
-
-    const expected = {
-      status: 200,
-      message: "Timeline found",
-      data: timeline,
-    };
-
-    const result = await sut.findById("timeline-id");
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
-  });
-
-  it("Should return 404 if timeline is not found during find timeline", async () => {
-    mockedTimelineRepository.findById.mockResolvedValue(null);
-
-    const expected = {
-      status: 404,
-      message: "Timeline not found",
-      data: null,
-    };
-
-    const result = await sut.findById("timeline-id");
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
-  });
-
-  it("Should return 500 if an error occurs in timelineRepository.findById during find timeline", async () => {
-    mockedTimelineRepository.findById.mockRejectedValue(new Error());
-
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
-
-    const result = await sut.findById("timeline-id");
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
-  });
-
-  it("Should update a timeline", async () => {
-    const payload = {
-      name: "Timeline 1",
-    };
-
-    const timeline = {
-      _id: "timeline-id",
-      name: "Timeline 1",
-    };
-
-    mockedTimelineRepository.update.mockResolvedValue(timeline);
-
-    const expected = {
-      status: 200,
-      message: "Timeline updated",
-      data: timeline,
-    };
-
-    const result = await sut.update("timeline-id", payload);
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.update).toBeCalledWith(
-      "timeline-id",
-      payload
-    );
-    expect(mockedTimelineRepository.update).toBeCalledTimes(1);
-  });
-
-  it("Should return 404 if timeline is not found during update a timeline", async () => {
-    const payload = {
-      name: "Timeline 1",
-    };
-
-    mockedTimelineRepository.update.mockResolvedValue(null);
-
-    const expected = {
-      status: 404,
-      message: "Timeline not found",
-      data: null,
-    };
-
-    const result = await sut.update("timeline-id", payload);
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.update).toBeCalledWith(
-      "timeline-id",
-      payload
-    );
-    expect(mockedTimelineRepository.update).toBeCalledTimes(1);
-  });
-
-  it("Should return 500 if an error occurs in timelineRepository.update during update a timeline", async () => {
-    const payload = {
-      name: "Timeline 1",
-    };
-
-    mockedTimelineRepository.update.mockRejectedValue(new Error());
-
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
-
-    const result = await sut.update("timeline-id", payload);
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.update).toBeCalledWith(
-      "timeline-id",
-      payload
-    );
-    expect(mockedTimelineRepository.update).toBeCalledTimes(1);
-  });
-
-  it("Should return all occurrences of a timeline", async () => {
+  describe("Find all occurrences", () => {
     const timeline = {
       _id: "timeline-id",
       name: "Timeline 1",
@@ -316,58 +283,60 @@ describe("Timeline service", () => {
 
     const occurrences = Array(10).fill({ name: "Occurrence 1" });
 
-    mockedTimelineRepository.findById.mockResolvedValue({
-      ...timeline,
-      populate: jest.fn(),
+    it("Should return all occurrences", async () => {
+      mockedTimelineRepository.findById.mockResolvedValue({
+        ...timeline,
+        populate: jest.fn(),
+      });
+
+      spyPaginate.mockReturnValue(occurrences);
+
+      const expected = {
+        status: 200,
+        message: "Occurrences found",
+        data: occurrences,
+      };
+
+      const result = await sut.findOccurrences("timeline-id", 1, 10);
+
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+      expect(spyPaginate).toBeCalledWith(timeline.occurrences, 1, 10);
+      expect(spyPaginate).toBeCalledTimes(1);
     });
 
-    spyPaginate.mockReturnValue(occurrences);
+    it("Should return 404 if timeline is not found", async () => {
+      mockedTimelineRepository.findById.mockResolvedValue(null);
 
-    const expected = {
-      status: 200,
-      message: "Occurrences found",
-      data: occurrences,
-    };
+      const expected = {
+        status: 404,
+        message: "Timeline not found",
+        data: null,
+      };
 
-    const result = await sut.findOccurrences("timeline-id", 1, 10);
+      const result = await sut.findOccurrences("timeline-id", 1, 10);
 
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
-    expect(spyPaginate).toBeCalledWith(timeline.occurrences, 1, 10);
-    expect(spyPaginate).toBeCalledTimes(1);
-  });
+      expect(result).toEqual(expected);
 
-  it("Should return 404 if timeline is not found during find occurrences of a timeline", async () => {
-    mockedTimelineRepository.findById.mockResolvedValue(null);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+    });
 
-    const expected = {
-      status: 404,
-      message: "Timeline not found",
-      data: null,
-    };
+    it("Should return 500 if an error occurs in timelineRepository.findById", async () => {
+      mockedTimelineRepository.findById.mockRejectedValue(new Error());
 
-    const result = await sut.findOccurrences("timeline-id", 1, 10);
+      const expected = {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
 
-    expect(result).toEqual(expected);
+      const result = await sut.findOccurrences("timeline-id", 1, 10);
 
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
-  });
-
-  it("Should return 500 if an error occurs in timelineRepository.findById during find occurrences of a timeline", async () => {
-    mockedTimelineRepository.findById.mockRejectedValue(new Error());
-
-    const expected = {
-      status: 500,
-      message: "Internal server error",
-      data: null,
-    };
-
-    const result = await sut.findOccurrences("timeline-id", 1, 10);
-
-    expect(result).toEqual(expected);
-    expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
-    expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+      expect(result).toEqual(expected);
+      expect(mockedTimelineRepository.findById).toBeCalledWith("timeline-id");
+      expect(mockedTimelineRepository.findById).toBeCalledTimes(1);
+    });
   });
 });
