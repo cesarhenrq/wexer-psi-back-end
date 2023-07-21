cd9d9be6d8172feca10dc09e8d70f0c558018aab
cesarhenrq <cesaarhvb@gmail.com>
2023-07-20 22:29:18 -0300

    feat: add delete

--- src/app/timelines/services/timeline.service.ts
+++ src/app/timelines/services/timeline.service.ts
@@ -1,5 +1,7 @@
 import TimelineRepository from "../repositories/timeline.repository";
 import PatientRepository from "../../patients/repositories/patient.repository";
+import OccurrenceRepository from "../../occurrences/repositories/occurrence.repository";
+import FileRepository from "../../files/repositories/file.repository";
 
 import { CreateTimelineDto } from "../dtos/create-timeline.dto";
 import { UpdateTimelineDto } from "../dtos/update-timeline.dto";
@@ -9,7 +11,9 @@ import paginate from "../../../utils/paginate";
 export default class TimelineService {
   constructor(
     private timelineRepository: TimelineRepository,
-    private patientRepository: PatientRepository
+    private patientRepository: PatientRepository,
+    private occurrenceRepository: OccurrenceRepository,
+    private fileRepository: FileRepository
   ) {}
 
   async create(patientId: string, payload: CreateTimelineDto) {
@@ -117,6 +121,63 @@ export default class TimelineService {
         status: 200,
         message: "Occurrences found",
         data: occurrences,
+      };
+    } catch (err) {
+      return {
+        status: 500,
+        message: "Internal server error",
+        data: null,
+      };
+    }
+  }
+
+  async delete(id: string, patientId: string) {
+    try {
+      const patient = await this.patientRepository.findById(patientId);
+
+      if (!patient) {
+        return {
+          status: 404,
+          message: "Patient not found",
+          data: null,
+        };
+      }
+
+      const timeline = await this.timelineRepository.delete(id);
+
+      if (!timeline) {
+        return {
+          status: 404,
+          message: "Timeline not found",
+          data: null,
+        };
+      }
+
+      if (timeline.occurrences.length) {
+        for (const occurrence of timeline.occurrences) {
+          const deletedOccurrence = await this.occurrenceRepository.delete(
+            occurrence._id as unknown as string
+          );
+
+          const hasFiles = deletedOccurrence && deletedOccurrence.files.length;
+
+          if (hasFiles) {
+            await this.fileRepository.deleteMany(
+              deletedOccurrence.files as unknown as string[]
+            );
+          }
+        }
+      }
+
+      await this.patientRepository.disassociateTimeline(
+        patientId,
+        timeline._id as unknown as string
+      );
+
+      return {
+        status: 200,
+        message: "Timeline deleted",
+        data: timeline,
       };
     } catch (err) {
       return {
