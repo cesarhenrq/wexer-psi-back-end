863cb06c0e586b93188c9a9d8e85ff7b18796089
cesarhenrq <cesaarhvb@gmail.com>
2023-07-21 22:16:38 -0300

    refactor: now all external repository are dependencies

--- src/app/users/user.module.ts
+++ src/app/users/user.module.ts
@@ -6,12 +6,26 @@ import UserService from "./services/user.service";
 
 import UserController from "./controllers/user.controller";
 
-import FileModule from "../files/file.module";
+import PatientRepository from "../patients/repositories/patient.repository";
+import TimelineRepository from "../timelines/repositories/timeline.repository";
+import OccurrenceRepository from "../occurrences/repositories/occurrence.repository";
+import FileRepository from "../files/repositories/file.repository";
 
 export default class UserModule {
-  static build() {
+  static build(
+    patientRepository: PatientRepository,
+    timelineRepository: TimelineRepository,
+    occurrenceRepository: OccurrenceRepository,
+    fileRepository: FileRepository
+  ) {
     const repository = new UserRepository(User);
-    const service = new UserService(repository, FileModule.build().repository);
+    const service = new UserService(
+      repository,
+      patientRepository,
+      timelineRepository,
+      occurrenceRepository,
+      fileRepository
+    );
     const controller = new UserController(service);
 
     return { controller, service, repository };
